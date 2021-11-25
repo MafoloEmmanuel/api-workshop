2b47659e748da36080ab52139e55c81cf5817ed0
MafoloEmmanuel <mafoloemmanuel1@gmail.com>
2021-11-25 14:59:21 +0200

    API thingies

--- public/carinfo.js
+++ public/carinfo.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const colorsElem = document.querySelector('.colors');
     const brandsElem = document.querySelector('.brands');
-    const carsElem = document.querySelector('.cars')
+    const carsElem = document.querySelector('.cars');
+
+    //compile handlebars
+
+
+    var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
+    // execute the compiled template and print the output to the console
+    console.log(template({ doesWhat: "rocks!" }));
+    template.innerHTML
 //Get all colors
 axios.get('http://api-tutor.herokuapp.com/v1/colors')
 .then((res)=>{
@@ -21,12 +29,19 @@ axios.get('http://api-tutor.herokuapp.com/v1/makes')
 //Get all cars
 axios.get('http://api-tutor.herokuapp.com/v1/cars')
 .then((res)=>{
-  var  carsResult = JSON.stringify(res.data)
-//console.log(carsResult) 
-carsElem.innerHTML=carsResult
+  //  var allCars = res.data
+ var  carsResult = JSON.stringify(res.data)
+carsElem.innerHTML =carsResult
+
+
+/*
+allCars.forEach(car => {
+  console.log(car.make='Toyota')
+  return car.make='Toyota'
+});
 //console.log(res.data)
 //console.log(res)
 //console.log(carsResult)
-
+*/
 })
 })
